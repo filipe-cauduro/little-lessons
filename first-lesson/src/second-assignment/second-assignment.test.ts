ab10364baacd3406277ad9a12a255522059c68f3
Filipe Melo <filipe.cauduro@outlook.com>
2025-02-23 01:23:40 -0300

    fix: fixes second-assignment tests

--- first-lesson/src/second-assignment/second-assignment.test.ts
+++ first-lesson/src/second-assignment/second-assignment.test.ts
@@ -2,22 +2,22 @@ import { describe, expect, it } from "vitest"
 import { averagePositiveNumbers } from "./second-assignment"
 
 describe("What's the average of the positive numbers in the list?", () => {
-  it("should return 42 given the list of numbers", () => {
+  it("should return 45.6 given the list of numbers", () => {
     const numbers = [49, 46, 74, 59, -6, -12, 0]
     const result = averagePositiveNumbers(numbers)
-    expect(result).toBe(42)
+    expect(result).toBe(45.6)
   })
 
-  it("should return 42 given the list of numbers, even with NaN values", () => {
+  it("should return 45.6 given the list of numbers, even with NaN values", () => {
     const numbers = [49, 46, 74, 59, -6, -12, 0, NaN]
     const result = averagePositiveNumbers(numbers)
-    expect(result).toBe(42)
+    expect(result).toBe(45.6)
   })
 
   it("should error if no positive number is informed", () => {
-    const numbers = [-49, -46, -74, 59, -6, -12]
+    const numbers = [-49, -46, -74, -59, -6, -12]
     expect(() => averagePositiveNumbers(numbers)).toThrowError(
-      "The informed list doesn't contain any positive numer",
+      "The informed list doesn't contain any positive number",
     )
   })
 })
